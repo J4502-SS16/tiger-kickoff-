27db6481f2d0c05e3be870ed73869f56e5364303
YinziZeng <wenqiangbo@Wenqiangs-MacBook-Air.local>
2016-05-01 18:45:27 -0700

    fix bugs and set slideup when window size down
    fix bugs and set the navigation slideup when the window's size is
    smaller than 700

--- menu.js
+++ menu.js
@@ -1,10 +1,15 @@
 $(document).ready(function(){
-  $ ('.menu-trigger').click(function(){
-    $('nav ul').slideToggle(500);
+  $ (".menu-trigger").click(function(){
+    $("#nav").slideToggle();
   });
   $(window).resize(function(){
     if($(window).width() > 700){
-      $('nav ul').removeAttr('style');
+      $("#nav").removeAttr("style");
         }
   });
+  $(window).resize(function() {
+    if($(window).width()<700) {
+      $("#nav").slideUp();
+    }
+  })
 });
